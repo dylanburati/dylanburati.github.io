6e20156bcda993e2bc5f58459e405da31d110a39
Dylan Burati <dylanburati@protonmail.com>
2019-10-01 11:29:37 -0400

    gatsby-site: add page title to blog post component

--- styles-2c91c26755f6ba2b3d3a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{213:function(n,w,o){}}]);
-//# sourceMappingURL=styles-2c91c26755f6ba2b3d3a.js.map
--- styles-827ca3d0f39980063cf2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{180:function(n,w,o){}}]);
-//# sourceMappingURL=styles-827ca3d0f39980063cf2.js.map
